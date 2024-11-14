2cc365ca92f6dd5d3198f2e205ae39c6c36c03a0
SayCV <sssxyc@163.com>
2024-11-14 14:07:59 +0800

    fix previous commit and try to parse from toml file

--- go.mod
+++ go.mod
@@ -1,14 +1,26 @@
 module github.com/saycv/ebomgen
 
-go 1.11
+go 1.18
 
 require (
 	github.com/PuerkitoBio/goquery v1.5.1
 	github.com/fedesog/webdriver v0.0.0-20180606182539-99f36c92eaef
 	github.com/larspensjo/config v0.0.0-20160228172812-b6db95dc6321
+	github.com/pelletier/go-toml v1.9.5
 	github.com/pkg/errors v0.8.0
 	github.com/prometheus/common v0.4.0
 	github.com/sirupsen/logrus v1.6.0
 	github.com/spf13/cobra v1.0.0
-	golang.org/x/net v0.7.0 // indirect
+)
+
+require (
+	github.com/alecthomas/template v0.0.0-20160405071501-a0175ee3bccc // indirect
+	github.com/alecthomas/units v0.0.0-20151022065526-2efee857e7cf // indirect
+	github.com/andybalholm/cascadia v1.1.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
+	github.com/spf13/pflag v1.0.3 // indirect
+	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
+	golang.org/x/sys v0.0.0-20190422165155-953cdadca894 // indirect
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6 // indirect
 )
--- pkg/parser/padslogic.go
+++ pkg/parser/padslogic.go
@@ -3,15 +3,23 @@ package parser
 import (
 	"bufio"
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"os"
+	"path"
+	"path/filepath"
 	"strings"
 
+	"github.com/pelletier/go-toml"
 	"github.com/saycv/ebomgen/pkg/types"
 	"github.com/saycv/ebomgen/pkg/utils"
 
 	log "github.com/sirupsen/logrus"
 )
+
+type SchConfig struct {
+	FpMap []any
+}
 
 // parse ascii text file to retrieve parts
 func parseTextParts(filename string) (map[string]types.EBOMItem, error) {
@@ -138,6 +146,28 @@ func ExtractPADSLogicComponents(filename string) ([]types.EBOMItem, error) {
 	}
 	defer input.Close()
 
+	dirname := filepath.Dir(filename)
+	basename := filepath.Base(filename)
+	suffix := filepath.Ext(filename)
+	filenameWithoutSuffix := strings.TrimSuffix(basename, suffix)
+	fnSlice := strings.Split(filenameWithoutSuffix, "-")
+
+	configfile_toml := path.Join(dirname, strings.Join(fnSlice[:1], "-")+"-metadata.toml")
+	if _, err := os.Stat(configfile_toml); err != nil {
+		configfile_toml = path.Join(dirname, fnSlice[0]+"-metadata.toml")
+	}
+	if _, err := os.Stat(configfile_toml); err != nil {
+		configfile_toml = path.Join(dirname, "metadata.toml")
+	}
+
+	var fpmaps []interface{}
+	if tree, err := toml.LoadFile(configfile_toml); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("Try to prase metadata from %s", configfile_toml)
+		fpmaps = tree.Get("FpMap").([]interface{})
+	}
+
 	parts, _ := parseTextParts(filename)
 
 	propfootprint := "unkownFp"
@@ -154,11 +184,24 @@ func ExtractPADSLogicComponents(filename string) ([]types.EBOMItem, error) {
 			propvalue = part.Value
 		}
 		if part.Footprint != "" {
-			if part.Footprint == "C0402_BGA" {
-				log.Infof("preprocess %v / %v / %v", part.Footprint, part.Value, part.References)
-				propfootprint = "C0402"
-				part.Footprint = propfootprint
+			// fmt.Printf("FpMap(): %s\n", fpmaps)
+			for _, entry := range fpmaps {
+				// 转换为数组类型
+				fpMapEntry := entry.([]interface{})
+				if len(fpMapEntry) == 2 {
+					// 获取包名和类型
+					packageOld := fpMapEntry[0].(string)
+					packageNew := fpMapEntry[1].(string)
+					//fmt.Printf("packageOld: %s, packageNew: %s\n", packageOld, packageNew)
+
+					if part.Footprint == packageOld {
+						log.Infof("preprocess %v / %v / %v", part.Footprint, part.Value, part.References)
+						propfootprint = packageNew
+						part.Footprint = propfootprint
+					}
+				}
 			}
+
 		}
 		if part.Attributes["Manufacturer Part Number"] != "" {
 			propclass["Manufacturer Part Number"] = part.Attributes["Manufacturer Part Number"]
